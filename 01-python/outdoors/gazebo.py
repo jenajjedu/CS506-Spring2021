bb8fada4846975027c2b000817cd405f6e34b803
Jena Jordahl <jenajj@bu.edu>
2021-02-08 00:13:58 -0500

    Updated the Outdoor Component to add Moore Lake to the City and a gazebo component with characteristics picked at random.

---
+++ 01-python/outdoors/gazebo.py
@@ -0,0 +1,6 @@
+import random
+
+def draw_gazebo():
+    gazebo_adjectives = ['pretty','windy', 'rain shelters', "shady"]
+    print("[Gazebos are " +random.choice(gazebo_adjectives)+ ".]")
+    return
--- 01-python/outdoors/outdoors.py
+++ 01-python/outdoors/outdoors.py
@@ -1,7 +1,9 @@
 from .lake import draw_lake
 from .park import draw_park
+from .gazebo import draw_gazebo
 
 def draw_outdoors():
     draw_lake()
     draw_park()
+    draw_gazebo()
     return
